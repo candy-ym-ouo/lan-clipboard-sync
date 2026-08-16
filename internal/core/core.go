2e3ee49c341ab26eb413a2c5dce0de4701b863de
xiaochangfeng <xcfouo@163.com>
2026-08-16 14:04:58 +0800

    Apply gold patch for task 002

--- internal/core/core.go
+++ internal/core/core.go
@@ -103,7 +103,7 @@ func (d *Devices) List(now time.Time) []Device {
 	defer d.mu.Unlock()
 	out := make([]Device, 0, len(d.values))
 	for id, device := range d.values {
-		if now.Sub(device.LastSeen) < d.ttl {
+		if device.LastSeen.Add(d.ttl).Before(now) {
 			delete(d.values, id)
 			continue
 		}
